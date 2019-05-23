cc1e5aed37e16455a2269981947d48854bfae559
Octavio Augusto Coria <octa.ac@gmail.com>
2019-05-22 22:46:20 -0300

    Completing workshop2 plot1, am in the time domain.

--- playground/workshops/workshop2/plot1_am.py
+++ playground/workshops/workshop2/plot1_am.py
@@ -8,15 +8,15 @@ from playground.utils.math_utils import sinusoide
 
 Ac = 5  # 5V
 Am = 2  # 2V
-Fc = 8  # 80KHz
-Fm = 0.4  # 40Hz
+Fc = 8  # 8Hz
+Fm = 0.4  # 0.4Hz
 
 time = numpy.arange(start=0, stop=10, step=0.01)
 
 modulated = go.Scatter(
     x=time,
     y=amplitude_modulation(Ac, Am, Fc, Fm, time),
-    name='AM',
+    name='Modulated signal',
 )
 
 carrier = go.Scatter(
@@ -28,16 +28,24 @@ carrier = go.Scatter(
 m_envelope = go.Scatter(
     x=time,
     y=sinusoide(Am, Fm, time, phase=0) + Ac,
-    name='M',
+    name='Envelope (A message + A carrier)',
 )
 m_envelope_2 = go.Scatter(
     x=time,
-    y=sinusoide(Am, Fm, time, phase=0) * -1 + Ac * -1,
-    name='M2',
+    y=(sinusoide(Am, Fm, time, phase=0) + Ac) * -1,
+    name='Envelope (A message + A carrier) * -1',
 )
 
+figure = go.Figure(
+    data=[modulated, m_envelope, m_envelope_2, carrier],
+    layout=go.Layout(
+        title='Modulated amplitude',
+        xaxis={'title': 'time (s)'},
+        yaxis={'title': 'A (V)'},
+    ),
+)
 plotly.offline.plot(
-    [modulated, m_envelope, m_envelope_2],
+    figure,
     filename='am_testing',
     auto_open=True,
 )
